c7345c7df821ba51d49c24ad52a52e2058d9bed2
s-desire.angeli@istitutopilati.it <s-desire.angeli@istitutopilati.it>
2025-10-23 15:07:10 +0200

    init

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 
-//using namespace std;
+using namespace std;
+
+class Counter{
+
+    private:
+        int value;
+
+    public:
+        Counter(){
+            cout <<"istanza dell'oggetto creata!\n"; 
+            value=0;
+        }
+        ~Counter(){
+            cout <<"oggetto distrutto!\n"; 
+        }     
+        int getValue(){
+            return value;
+        }
+        void increase(int n){
+            value+=n;
+        
+        }
+};
+
 int main(){
-    std::cout <<"ciamo mondo!";
+
+    Counter c1,c2;
+    
+    cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    cout << "Contatore 2: "<< c2.getValue() <<"\n";
+
+    c1.increase(7);
+
+    cout << "Contatore 1: "<< c1.getValue() <<"\n";
+    cout << "Contatore 2: "<< c2.getValue() <<"\n";
+
 
     return 0;
 }
